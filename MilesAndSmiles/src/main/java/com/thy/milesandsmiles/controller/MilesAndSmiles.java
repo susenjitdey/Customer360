f90fcbfd44b07a3068950cee485bed7a2ee323d4
SusenjitDey <SusenjitDey@DESKTOP-GR4UNS9>
2019-08-14 12:59:48 +0530

    First commit for MilesAndSmiles

---
+++ MilesAndSmiles/src/main/java/com/thy/milesandsmiles/controller/MilesAndSmiles.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.thy.milesandsmiles.controller;
+
+/**
+ * @author SusenjitDey
+ *
+ */
+public class MilesAndSmiles {
+	
+	//Member Tier Expiration{}
+	//Miles to the Next Tier{}
+	//Status Miles{}
+	//Bonus Miles{}
+
+
+}
