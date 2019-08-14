763a64bd8dc4ea8e29a87d3b5e94580bf8c82d50
SusenjitDey <SusenjitDey@DESKTOP-GR4UNS9>
2019-08-14 12:59:09 +0530

    FirstCommit for FlightInformation

---
+++ FlightInformation/src/main/java/com/thy/flightinformation/FlightInformationApplication.java
@@ -0,0 +1,13 @@
+package com.thy.flightinformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightInformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightInformationApplication.class, args);
+	}
+
+}
