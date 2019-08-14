6f71b357957ee7aeafc525ecc232e85ee595fc30
SusenjitDey <SusenjitDey@DESKTOP-GR4UNS9>
2019-08-14 12:57:52 +0530

    First commit for Feebbacks

---
+++ Feedbacks/src/main/java/com/thy/feedbacks/FeedbacksApplication.java
@@ -0,0 +1,13 @@
+package com.thy.feedbacks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbacksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbacksApplication.class, args);
+	}
+
+}
